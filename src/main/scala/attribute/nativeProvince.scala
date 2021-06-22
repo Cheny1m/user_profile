ea14713c20d8b0123242ea808340e425bfbcf993
2018070828 <205431414@qq.com>
2021-06-22 16:41:22 +0800

    人口属性：籍贯（市，省）
    This reverts commit 3f9382b0

--- src/main/scala/attribute/nativeProvince.scala
+++ src/main/scala/attribute/nativeProvince.scala
@@ -11,7 +11,7 @@ import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
 
 
 
-object nativePlace {
+object nativeProvince {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("gender")
@@ -20,7 +20,7 @@ object nativePlace {
 
     import  spark.implicits._
 
-//    读数据
+    //    读数据
     def catalog =
       """{
         |"table":{"namespace":"default","name":"tbl_users"},
@@ -34,11 +34,11 @@ object nativePlace {
       .option(HBaseTableCatalog.tableCatalog, catalog)
       .format("org.apache.spark.sql.execution.datasources.hbase")
       .load()
-//    readDF.show()
+    //    readDF.show()
 
 
-//    数据处理
-//    查询归属地,调用阿里云api
+    //    数据处理
+    //    查询归属地,调用阿里云api
     val getCity= (mobile: String)=> {
       val appcode = "244e9f8e5da04ce8b36568fa6d0fa92a"
       val httpClient = HttpClients.createDefault()    // 创建 client 实例
@@ -48,28 +48,26 @@ object nativePlace {
       get.setHeader("Content-Type", "application/json; charset=UTF-8")
       val response = httpClient.execute(get)    // 发送请求
       var jsonOBJ :JSONObject = JSON.parseObject(EntityUtils.toString(response.getEntity) )
-//      println(mobile + jsonOBJ.getString("msg"))
       jsonOBJ  =  JSON.parseObject(jsonOBJ.getString("result") )
-//      println(jsonOBJ)
-      val res = jsonOBJ.getString("city")
+      val res = jsonOBJ.getString("province")
       if(res == "") "未知" else res
     }
-//    查询每个手机归属地
+    //    查询每个手机归属地
     val resultDF = readDF.rdd.map(row => {
       val s1 = row.getAs[String](0)
       val s2 = row.getAs[String](1)
       (s1,getCity(s2))
-    }).toDF("id","nativePlace")
+    }).toDF("id","nativeProvince")
 //    resultDF.show(950)
 
-//    写数据
+    //    写数据
     def catalogwrite =
       """{
         |"table":{"namespace":"default","name":"user_profile"},
         |"rowkey":"id",
         |"columns":{
         |"id":{"cf":"rowkey","col":"id","type":"string"},
-        |"nativePlace":{"cf":"cf","col":"nativePlace","type":"string"}
+        |"nativeProvince":{"cf":"cf","col":"nativeProvince","type":"string"}
         |}}
       """.stripMargin
     resultDF.write
@@ -77,12 +75,23 @@ object nativePlace {
       .format("org.apache.spark.sql.execution.datasources.hbase")
       .save()
 
-//    查看运行结果，要先注释上面的数据处理和写数据操作
-//    spark.read
-//      .option(HBaseTableCatalog.tableCatalog, catalogwrite)
-//      .format("org.apache.spark.sql.execution.datasources.hbase")
-//      .load()
-//      .show(950)
+
+//    查看运行结果，要先注释上面的数据处理和写数据
+//    def catalogres =
+//      """{
+//        |"table":{"namespace":"default","name":"user_profile"},
+//        |"rowkey":"id",
+//        |"columns":{
+//        |"id":{"cf":"rowkey","col":"id","type":"string"},
+//        |"nativeProvince":{"cf":"cf","col":"nativeProvince","type":"string"},
+//        |"nativePlace":{"cf":"cf","col":"nativePlace","type":"string"}
+//        |}}
+//      """.stripMargin
+//        spark.read
+//          .option(HBaseTableCatalog.tableCatalog, catalogres)
+//          .format("org.apache.spark.sql.execution.datasources.hbase")
+//          .load()
+//          .show(950)
 
 
 
