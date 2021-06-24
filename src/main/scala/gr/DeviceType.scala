8c4ebd93612600e65b913bd9bac7de0d9edfb8c8
2018110114 <1975172025@qq.com>
2021-06-24 11:38:32 +0800

    设备类型，调试

---
+++ src/main/scala/gr/DeviceType.scala
@@ -0,0 +1,41 @@
+package gr
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
+
+/**
+  * @author Tracy
+  * @date 2021/6/23 15:16
+  */
+object DeviceType {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("lastLogin")
+      .master("local")
+      .getOrCreate()
+
+    //    读数据
+    def catalog =
+      """{
+        |"table":{"namespace":"default","name":"tbl_logs"},
+        |"rowkey":"id",
+        |"columns":{
+        |"id":{"cf":"rowkey","col":"id","type":"string"},
+        |"global_user_id":{"cf":"cf","col":"global_user_id","type":"string"},
+        |"user_agent":{"cf":"cf","col":"user_agent","type":"string"}
+        |}}
+      """.stripMargin
+
+    val readDF = spark.read
+      .option(HBaseTableCatalog.tableCatalog, catalog)
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .load()
+
+    readDF.show(20)
+
+    readDF.rdd.foreach(row => {
+      val user_agent = row.getString(2)
+      println(user_agent)
+    })
+  }
+}
