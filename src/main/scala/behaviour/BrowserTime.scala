5aff4f12f3cca839a0347f3a43addf4268b77786
2018070828 <205431414@qq.com>
2021-06-26 19:38:01 +0800

    修改包名：gr -> behaviour
    This reverts commit 3f9382b0

--- src/main/scala/behaviour/BrowserTime.scala
+++ src/main/scala/behaviour/BrowserTime.scala
@@ -1,13 +1,13 @@
-package gr
+package behaviour
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
 
 /**
   * @author Tracy
-  * @date 2021/6/23 15:52
+  * @date 2021/6/23 10:22
   */
-object LoginFrequency {
+object BrowserTime {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("lastLogin")
@@ -21,8 +21,8 @@ object LoginFrequency {
         |"rowkey":"id",
         |"columns":{
         |"id":{"cf":"rowkey","col":"id","type":"string"},
-        |"global_user_id":{"cf":"cf","col":"global_user_id","type":"string"},
-        |"user_agent":{"cf":"cf","col":"user_agent","type":"string"}
+        |"log_time":{"cf":"cf","col":"log_time","type":"string"},
+        |"log_id":{"cf":"cf","col":"log_id","type":"string"}
         |}}
       """.stripMargin
 
--- src/main/scala/behaviour/LoginFrequency.scala
+++ src/main/scala/behaviour/LoginFrequency.scala
@@ -1,13 +1,13 @@
-package gr
+package behaviour
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
 
 /**
   * @author Tracy
-  * @date 2021/6/23 10:22
+  * @date 2021/6/23 15:52
   */
-object BrowserTime {
+object LoginFrequency {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("lastLogin")
@@ -21,8 +21,8 @@ object BrowserTime {
         |"rowkey":"id",
         |"columns":{
         |"id":{"cf":"rowkey","col":"id","type":"string"},
-        |"log_time":{"cf":"cf","col":"log_time","type":"string"},
-        |"log_id":{"cf":"cf","col":"log_id","type":"string"}
+        |"global_user_id":{"cf":"cf","col":"global_user_id","type":"string"},
+        |"user_agent":{"cf":"cf","col":"user_agent","type":"string"}
         |}}
       """.stripMargin
 
